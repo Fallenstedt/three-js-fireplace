e69464e53d65bf818323447c924d2eb11389f2c5
Alex Fallenstedt <a.fallenstedt@gmail.com>
2017-09-01 22:45:18 -0700

    oopsie

--- app/fireCube.js
+++ app/fireCube.js
@@ -8,6 +8,7 @@ class FireCube {
     this.geometry = new THREE.BoxGeometry( 1, 1, 1)
     this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
     this.mesh = new THREE.Mesh(this.geometry, this.material);
+    // this.mesh.position()
 
     this.animationDuration = CONFIG.animDuration;
     this.animationDelay = getRandomArbitrary(CONFIG.animDelayMin, CONFIG.animDelayMax)
